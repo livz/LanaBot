ad2e543fb522a0435c55f916900dbeb00a49c189
livz <liviu.itoafa@gmail.com>
2017-02-16 21:02:01 +0000

    Add weather functionality

--- lanabot.py
+++ lanabot.py
@@ -6,45 +6,55 @@ import ConfigParser
 import logging
 
 TOKEN = ""
+OWM_KEY = ""
 POLLING_TIMEOUT = None
 
-# Lambda functions to parse updates
+# Lambda functions to parse updates from Telegram
 def getText(update):            return update["message"]["text"]
+def getLocation(update):        return update["message"]["location"]
 def getChatId(update):          return update["message"]["chat"]["id"]
 def getUpId(update):            return int(update["update_id"])
 def getResult(updates):         return updates["result"]
 
+# # Lambda functions to parse weather responses
+def getDesc(w):         return w["weather"][0]["description"]
+def getTemp(w):         return w["main"]["temp"]
+def getCity(w):         return w["name"]
+
 logger = logging.getLogger("LanaBot")
 logger.setLevel(logging.DEBUG)
+
+# Cities for weather requests
+cities = ["London", "Brasov"]
 
 # Configure file and console logging
 def configLogging():
     # Create file logger and set level to DEBUG
     # Mode = write -> clear existing log file
-    handler = logging.FileHandler('run.log', mode='w')
+    handler = logging.FileHandler("run.log", mode="w")
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
     # Create console handler and set level to INFO
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
-    formatter = logging.Formatter('[%(levelname)s] - %(message)s')
+    formatter = logging.Formatter("[%(levelname)s] - %(message)s")
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
 # Read settings from configuration file
 def parseConfig():
-    global URL, POLLING_TIMEOUT
+    global URL, URL_OWM, POLLING_TIMEOUT
     
     c = ConfigParser.ConfigParser()
-    c.read('config.ini')
-    TOKEN = c.get('Settings', 'TOKEN')
-    logger.debug("Token: %s" % TOKEN)
+    c.read("config.ini")
+    TOKEN = c.get("Settings", "TOKEN")
     URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-    POLLING_TIMEOUT = c.get('Settings', 'POLLING_TIMEOUT')
-    logger.debug("Polling interval: %s" % POLLING_TIMEOUT)
+    OWM_KEY = c.get("Settings", "OWM_KEY")
+    URL_OWM = "http://api.openweathermap.org/data/2.5/weather?appid={}&units=metric".format(OWM_KEY)
+    POLLING_TIMEOUT = c.get("Settings", "POLLING_TIMEOUT")
 
 # Make a request to Telegram bot and get JSON response
 def makeRequest(url):
@@ -54,7 +64,7 @@ def makeRequest(url):
     return resp
 
 # Return all the updates with ID > offset
-# Updates list kept by Telegram for 24h
+# (Updates list is kept by Telegram for 24h)
 def getUpdates(offset=None):
     url = URL + "getUpdates?timeout=%s" % POLLING_TIMEOUT
     logger.info("Getting updates") 
@@ -63,10 +73,44 @@ def getUpdates(offset=None):
     js = makeRequest(url)
     return js
 
+# Build a one-time keyboard for on-screen options
+def buildKeyboard(items):
+    keyboard = [[{"text":item}] for item in items]
+    replyKeyboard = {"keyboard":keyboard, "one_time_keyboard": True}
+    logger.debug(replyKeyboard)
+    return json.dumps(replyKeyboard)
+
+def buildCitiesKeyboard():
+    keyboard = [[{"text": c}] for c in cities]
+    keyboard.append([{"text": "Share location", "request_location": True}])
+    replyKeyboard = {"keyboard": keyboard, "one_time_keyboard": True}
+    logger.debug(replyKeyboard)
+    return json.dumps(replyKeyboard)
+
+# Query OWM for the weather for place or coords
+def getWeather(place):
+    print type(place)
+    if isinstance(place, dict):     # coordinates provided
+        lat, lon = place["latitude"], place["longitude"]
+        url = URL_OWM + "&lat=%f&lon=%f&cnt=1" % (lat, lon)
+        logger.info("Requesting weather: " + url)
+        js = makeRequest(url)
+        logger.debug(js)
+        return "%s degrees, %s in %s" % (getTemp(js), getDesc(js), getCity(js))
+    else:                           # place name provided 
+        # make req
+        url = URL_OWM + "&q={}".format(place)
+        logger.info("Requesting weather: " + url)
+        js = makeRequest(url)
+        logger.debug(js)
+        return "%s degrees, %s in %s" % (getTemp(js), getDesc(js), getCity(js))
+
 # Send URL-encoded message to chat id
-def sendMessage(text, chatId):
+def sendMessage(text, chatId, interface=None):
     text = urllib.quote_plus(text)
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chatId)
+    url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chatId)
+    if interface:
+        url += "&reply_markup={}".format(interface)
     requests.get(url)
 
 # Get the ID of the last available update
@@ -76,12 +120,42 @@ def getLastUpdateId(updates):
         ids.append(getUpId(update))
     return max(ids)
 
+# Keep track of conversation states: 'weatherReq'
+chats = {}
+
 # Echo all messages back
-def echoAll(updates):
+def handleUpdates(updates):
     for update in getResult(updates):
-        text = getText(update)
         chatId = getChatId(update)
-        sendMessage(text, chatId)
+        try:
+            text = getText(update)
+        except Exception as e:
+            logger.error("No text field in update. Try to get location")
+            loc = getLocation(update)
+            # Was weather previously requested?
+            if (chatId in chats) and (chats[chatId] == "weatherReq"):
+                logger.info("Weather requested for %s in chat id %d" % (str(loc), chatId))
+                # Send weather to chat id and clear state
+                sendMessage(getWeather(loc), chatId)
+                del chats[chatId]
+            continue
+
+        if text == "/weather":
+            keyboard = buildCitiesKeyboard()
+            chats[chatId] = "weatherReq"
+            sendMessage("Select a city", chatId, keyboard)
+        elif text.startswith("/"):
+            logger.warning("Invalid command %s" % text)    
+            continue
+        elif (text in cities) and (chatId in chats) and (chats[chatId] == "weatherReq"):
+            logger.info("Weather requested for %s" % text)
+            # Send weather to chat id and clear state
+            sendMessage(getWeather(text), chatId)
+            del chats[chatId]
+        else:
+            keyboard = buildKeyboard(["/weather"])
+            sendMessage("Meowwwww I'm not that advanced. I only know these commands", 
+                chatId, keyboard)
 
 def main():
     configLogging()
@@ -97,7 +171,7 @@ def main():
         updates = getUpdates(last_update_id)
         if len(getResult(updates)) > 0:
             last_update_id = getLastUpdateId(updates) + 1
-            echoAll(updates)
+            handleUpdates(updates)
         time.sleep(0.5)
 
 if __name__ == "__main__":
